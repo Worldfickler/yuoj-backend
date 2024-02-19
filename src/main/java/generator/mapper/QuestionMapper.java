567b85263b5e686bd0eb69d00317f98b4ead855c
Fickler <2586844575@qq.com>
2024-02-19 15:10:28 +0800

    Initial commit

---
+++ src/main/java/generator/mapper/QuestionMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.Question;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author dell
+* @description 针对表【question(题目)】的数据库操作Mapper
+* @createDate 2024-02-19 15:07:10
+* @Entity generator.domain.Question
+*/
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+
---
+++ src/main/java/generator/service/QuestionService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.Question;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author dell
+* @description 针对表【question(题目)】的数据库操作Service
+* @createDate 2024-02-19 15:07:10
+*/
+public interface QuestionService extends IService<Question> {
+
+}
